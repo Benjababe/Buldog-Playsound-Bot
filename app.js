483d0c1a63cc1f17598fedc08698a442a7098c8c
Benjababe <benjamin2471@gmail.com>
2020-10-06 16:35:45 +0000

    removed glitch stuff

--- app.js
+++ app.js
@@ -141,21 +141,3 @@ let handlePlaysoundTable = table => {
 
 setInterval(updatePlaysounds, 86400000);
 //updatePlaysounds();
-
-//-------------------------GLITCH WEBSTUFF-------------------------//
-
-console.log("Allo playsound bot is up and running");
-
-const http = require("http"),
-  express = require("express"),
-  app = express();
-app.get("/", (req, res) => {
-  console.log(`${Date.now()} Ping Received`);
-  res.sendStatus(200);
-});
-
-app.listen(3000);
-
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
